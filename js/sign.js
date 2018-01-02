c7a590a8fbb5ba218c5cc42d8c58bf58c5d9199b
Lauraisa <galindolaura.isabel@gmail.com>
2018-01-02 12:22:06 -0500

    agregfando funcionalidad a signup

--- js/sign.js
+++ js/sign.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   $("#countries").msDropdown();
  
-  $("#countries").select(function () {
-    if (('#countries').eq(0).select =$('#peru') ) {
-      $('div#celInp').append('input').text('55');
+  $("#countries").change(function () {
+    if ($(this).val() === 'peru' ){
+      $('#celInp').val('+51');
+      }
+    else if ($(this).val() === 'mexico' ){
+      $('#celInp').val('+52');
+      }
+    else if ($(this).val() === 'colombia' ){
+      $('#celInp').val('+57');
     }
- 
-  });
-  
+    else {
+      ($(this).val() === 'eu')
+      $('#celInp').val('+1');
+      }
+    })
   });
